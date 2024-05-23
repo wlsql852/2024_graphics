b481e82685350e05a579e691a4e6545971fedb69
wlsql852 <108854643+wlsql852@users.noreply.github.com>
2024-05-23 18:58:10 +0900

    5th - start

--- hw/04.Camera/cpp/skeleton/function.cpp
+++ hw/04.Camera/cpp/skeleton/function.cpp
@@ -1,3 +1,4 @@
-// cd hw/04.Camera/cpp/skeleton : 해당 파일로 이동
-// make : 결과 실행 파일 생성
-//./camera : 결과 파일 출력
+/* cd hw/04.Camera/cpp/skeleton : 해당 파일로 이동
+ make : 결과 실행 파일 생성
+./camera : 결과 파일 출력
+*/
---
+++ hw/05.hello_assimp/cpp/skeleton/function.cpp
@@ -0,0 +1,5 @@
+/*
+cd hw/05.hello_assimp/cpp/skeleton : 해당 폴더 이동
+make : 결과 실행 파일 생성
+./helloassimp : 결과 파일 출력
+*/
